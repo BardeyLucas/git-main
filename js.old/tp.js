1844ecc7cc3073a2384ed7110f756337436233ed
Denki <bardey.estelle1@gmail.com>
2025-11-19 13:28:40 +0100

    Changement de tp.js

--- js.old/tp.js
+++ js.old/tp.js
@@ -1,125 +1,132 @@
+//1. Manipuler des variables
+
 let gold = 100;
 
-console.log(gold);
+console.log(`tu as ${gold} pieces d'or`);
 
-gold = gold + 50;
-console.log(gold);
+gold += 50;
 
-gold = gold - 30;
-console.log(gold);
+console.log(`tu as maintenant ${gold} pieces d'or`);
 
-const kingdomName = "Mon royaume";
+gold -= 30;
+
+console.log(`après avoir acheté une épée, il te reste ${gold} pieces d'or`);
+
+const kingdomName = "Rive d'asure";
+
+console.log(`Bienvenue dans le royaume de ${kingdomName}`);
+
+// changer la valeur d'une constante créer une erreur
+
+//Travailler avec des tableaux
 
 const quests = [
-  "Sauver le chat du village",
-  "Explorer la forêt sombre",
-  "Boire la potion magique",
+  "Sauver le poisson-chat du village",
+  "Explorer la mer sombre",
   "Trouver la potion magique",
-  "Chasser les gobelins",
+  "Chasser les squelettes",
 ];
 
-console.log(quests[0]);
+console.log('Commencer par la quête : ' + quests[0]);
 
-quests.push("Parler au sage de la montagne");
+quests.push("Parler au gérant de la taverne");
 
-console.log(quests);
+const QuestWithLa = quests.filter(quest => quest.includes(" la "));
 
-const filteredQuests = quests.filter((quest) => quest.includes(" la "));
-console.log(filteredQuests);
+console.log(`Regarder aussi ces quêtes : ${QuestWithLa}`);
 
-const potionQuest = quests.findLast((toto) => toto.includes("potion"));
-console.log(potionQuest);
+const QuestWithPotion = quests.filter(quest => quest.includes(" potion "));
 
-function heal(player) {
-  const newLife = player.life + 20;
+console.log(`Tu cherche la fameuse potion, consulte donc la quête : ${QuestWithPotion}`)
 
-  if (newLife > 100) {
-    player.life = 100;
-  } else {
-    player.life = newLife;
-  }
-
-  return player;
-}
+//Gérer des objets
 
 const player = {
-  name: "Aeris",
-  gold: 100,
-  level: 1,
-  life: 100,
-  xp: 0,
-  inventory: ["épée"],
-};
+    name: "Captaine Barbe Sombre",
+    level: 1,
+    health: 100,
+    inventory: ["Perroquet bavard"],
+}
 
-player.level = 5;
+console.log('Les statistiques du joueur sont : ' + player.name + ', est un pirate niveau ' + player.level + ', il lui reste ' + player.health + ' pv et il possède dans son inventaire : ' + player.inventory);
 
-player.inventory.push("bouclier");
+player.level += 1;
 
-player.life = 60;
+console.log('Vous passez au niveau ' + player.level + ' !');
+
+player.health -= 20;
+
+console.log('Vous subissez des dégâts, il vous reste ' + player.health + ' pv !');
+
 player.isAlive = true;
 
-heal(player);
-heal(player);
-heal(player);
+console.log(`Le joueur est-il en vie ? ${player.isAlive}`);
 
-const damage = (player, amount) => {
-  const newLife = player.life - amount;
-  if (newLife <= 0) {
-    player.life = 0;
-    player.isAlive = false;
-  } else {
-    player.life = newLife;
-  }
-  return player;
-};
+//Créer une fonction
 
-console.log(damage(player, 50));
-console.log(damage(player, 30));
-console.log(damage(player, 20));
+function heal(player) {
+    if (player.isAlive) {
+        if (player.health <= 80) {
+            player.health += 20;
+        } else {
+            player.health = 100;
+        }
+    }
+    return player;
+}
+
+function damage(player, amount) {
+    player.health -= amount;
+    if (player.health <= 0) {
+        player.health = 0;
+        player.isAlive = false;
+    }
+    return player;
+}
+
+//Filtrer des objets
 
 const items = [
-  { name: "Potion", price: 10, rare: false },
-  { name: "Épée en fer", price: 50, rare: false },
-  { name: "Arc elfique", price: 120, rare: true },
-  { name: "Armure d’or", price: 300, rare: true },
-];
+    { name: "PANZER (l'épée)", price: 10, rare: false},
+    { name: "Matthilde (chapeau)", price: 50, rare: false },
+    { name: "La faible (épée laser)", price: 120, rare: true },
+    { name: "L’Obscur Parangon des Hurle-Plumes (juste un perroquet)", price: 300, rare: true }
+]
 
-// const rareItems = items.filter((item) => item.rare);
-// console.log(rareItems);
+console.log('Objets disponibles : \n' + items.map(item => item.name).join(',\n'));
 
-// const cheapItems = items.filter((item) => item.price < 100);
-// console.log(cheapItems);
-// const weapon = items.find((item) => (item.name = "Arc elfique"));
+const rareItems  = items.filter(item => item.rare==true);
 
-function buy(player, weapon) {
-  const selectedWeapon = items.find((item) => item.name === weapon);
-  console.log(selectedWeapon);
-  console.log(selectedWeapon.price, player.gold);
+console.log('Objets rares : \n' + rareItems.map(item => item.name).join(',\n'));
 
-  if (selectedWeapon.price > player.gold) {
-    console.log("Pas assez de gold");
-  } else {
-    player.inventory.push(selectedWeapon.name);
-    player.gold = player.gold - selectedWeapon.price;
-  }
+const ArcItems  = items.filter(item => item.name=="L’Obscur Parangon des Hurle-Plumes (juste un perroquet)");
 
-  return player;
+console.log('Objets demander : \n' + ArcItems.map(item => item.name).join(',\n'));
+
+//Système de boutique
+
+player.gold = 150;
+
+function buy(player, item) {
+    if (player.gold >= item.price) {
+        player.gold -= item.price;
+        player.inventory.push(item.name);
+        console.log(`Vasy prend moi ce ${item.name} que t'a acheter ${item.price} et profite en temps que tu peux petit gars.`);
+    } else {
+        console.log("Sale pauvre");
+    }
+    return player;
 }
 
-console.log(buy(player, "ezaeaze"));
-console.log(buy(player, "Potion"));
+//Le système d’EXP
 
-function gainXP(player, amount) {
-  const newXp = player.xp + amount;
-  if (newXp >= 100) {
-    player.level++;
-    player.xp = amount - player.xp;
-  } else {
-    player.xp = newXp;
-  }
-  return player;
+player.xp = 0;
+
+function gainXp(player, amount) {
+    player.xp += amount;
+    while (player.xp >= 100) {
+        player.xp -= 100;
+        player.level += 1;
+        console.log(`Félicitation ! Vous avez atteint le niveau ${player.level} !`);
+    }
 }
-
-console.log(gainXP(player, 50));
-console.log(gainXP(player, 100));
-console.log(gainXP(player, 50));
